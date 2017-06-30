c32156b566432d966c638b78f511eca8913a2603
Ben McClure <ben.mcclure@gmail.com>
2017-06-30 01:56:59 -0500

    Allow passing defaults into the singleton which are only used when initializing it

--- singleton.js
+++ singleton.js
@@ -1,11 +1,9 @@
 var Conf = require('./index')
 var config
 
-module.exports = (cfg) => {
-    if (typeof cfg !== 'undefined') {
-        config = new Conf(cfg)
-    } else if (typeof config === 'undefined') {
-        config = new Conf()
+module.exports = (cfg, defaults) => {
+    if (typeof cfg !== 'undefined' || typeof config === 'undefined') {
+        config = new Conf(Object.assign(defaults || {}, cfg))
     }
 
     return config
