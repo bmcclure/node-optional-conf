4bdc3b3c12d8a79bed47ab264df432bf9132afb3
Ben McClure <ben.mcclure@gmail.com>
2017-06-30 02:11:42 -0500

    Switch defaults parameter to forceInitialize in singleton

--- singleton.js
+++ singleton.js
@@ -1,9 +1,9 @@
 var Conf = require('./index')
 var config
 
-module.exports = (cfg, defaults) => {
-    if (typeof cfg !== 'undefined' || typeof config === 'undefined') {
-        config = new Conf(Object.assign(defaults || {}, cfg))
+module.exports = (cfg, forceInitialize) => {
+    if (forceInitialize || typeof config === 'undefined') {
+        config = new Conf(cfg)
     }
 
     return config
